46d376f42255921e0f5e54b14d559abf30be27d6
Kimbeaux <KimbeauxE@verizon.net>
2012-03-06 18:12:44 -0500

    added extra credit 4 and 5

--- ex3.rb
+++ ex3.rb
@@ -2,26 +2,26 @@
 puts "I will now count my chickens:"
 
 # Print "Hens" to screen, calculates 30 / 6, then 25 + 5.
-puts "Hens", 25 + 30 / 6
+puts "Hens", 25.0 + 30.0 / 6.0
 # Print "Roosters" to screen, calculates 25 * 3, then remainder of 75 / 4, then 100 - 3.
-puts "Roosters", 100 - 25 * 3 % 4
+puts "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
 # Print "Now I will count the eggs:" to screen.
 puts "Now I will count the eggs:"
 
 # Calculate remainder of 4/2 = 0, then divides 1/4 = 0, then sums 3 + 2 + 1 - 5 + 0 - 0 + 6.
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # Print "Is it true that 3 + 2 < 5 - 7?" to screen.
-puts "Is it true that 3 + 2 < 5 - 7?"
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
 
 # Print result of operation 3 + 2 < 5 - 7 to screen.
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2.0 < 5.0 - 7.0
 
 # Print "What is 3 + 2?", print result 3 + 2.
-puts "What is 3 + 2?", 3 + 2
+puts "What is 3 + 2?", 3.0 + 2.0
 # Print "What is 5 - 7?", print result 5 - 7.
-puts "What is 5 - 7?", 5 - 7
+puts "What is 5 - 7?", 5.0 - 7.0
 
 # Print "Oh, that's why it's false."
 puts "Oh, that's why it's false."
@@ -30,8 +30,8 @@ puts "Oh, that's why it's false."
 puts "How about some more."
 
 # Print "Is it greater?", evaluate 5 > -2
-puts "Is it greater?", 5 > -2
+puts "Is it greater?", 5.0 > -2.0
 # Print "Is it greater or equal?", evaluate 5>= -2
-puts "Is it greater or equal?", 5 >= -2
+puts "Is it greater or equal?", 5.0 >= -2.0
 # Print Is it less or equal?", evaluate 5 <= -2
-puts "Is it less or equal?", 5 <= -2
+puts "Is it less or equal?", 5.0 <= -2.0
