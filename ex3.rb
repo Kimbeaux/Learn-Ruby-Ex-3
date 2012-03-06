4874a9123612ea1028797c11001c655c56efad95
Kimbeaux <KimbeauxE@verizon.net>
2012-03-06 17:41:56 -0500

    added extra credit 1-3

--- ex3.rb
+++ ex3.rb
@@ -1,23 +1,37 @@
+# Print "I will now count my chickens:" to screen.
 puts "I will now count my chickens:"
 
+# Print "Hens" to screen, calculates 30 / 6, then 25 + 5.
 puts "Hens", 25 + 30 / 6
+# Print "Roosters" to screen, calculates 25 * 3, then remainder of 75 / 4, then 100 - 3.
 puts "Roosters", 100 - 25 * 3 % 4
 
+# Print "Now I will count the eggs:" to screen.
 puts "Now I will count the eggs:"
 
+# Calculate remainder of 4/2 = 0, then divides 1/4 = 0, then sums 3 + 2 + 1 - 5 + 0 - 0 + 6.
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# Print "Is it true that 3 + 2 < 5 - 7?" to screen.
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+# Print result of operation 3 + 2 < 5 - 7 to screen.
 puts 3 + 2 < 5 - 7
 
+# Print "What is 3 + 2?", print result 3 + 2.
 puts "What is 3 + 2?", 3 + 2
+# Print "What is 5 - 7?", print result 5 - 7.
 puts "What is 5 - 7?", 5 - 7
 
+# Print "Oh, that's why it's false."
 puts "Oh, that's why it's false."
 
+# Print "How about some more."  Note:  . sb ?
 puts "How about some more."
 
+# Print "Is it greater?", evaluate 5 > -2
 puts "Is it greater?", 5 > -2
+# Print "Is it greater or equal?", evaluate 5>= -2
 puts "Is it greater or equal?", 5 >= -2
+# Print Is it less or equal?", evaluate 5 <= -2
 puts "Is it less or equal?", 5 <= -2
